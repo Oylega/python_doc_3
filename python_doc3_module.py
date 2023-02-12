475a6cb7fafb872bb24dff22e10159288e43e5a0
Oylega <117492599+Oylega@users.noreply.github.com>
2023-02-11 19:39:48 -0500

    Add files via upload

---
+++ python_doc3_module.py
@@ -0,0 +1,18 @@
+
+def find_area():
+    length = int(input("What is the area of the house?\n"))
+    width = int(input("What is the width of the house?\n"))
+    area = length * width
+    return(area)
+
+
+ 
+
+def find_circumference():
+    import math
+    pi = math.pi
+    radius = int(input("What is the radius of the circle?\n"))
+    circumference = 2 * pi * radius
+    return(circumference)
+
+
